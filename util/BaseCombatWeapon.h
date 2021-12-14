782e3a188085c928c2b714d2a88c8befc55559c9
Ted Wang <ttwang@ttwang-PC.lan>
2021-12-14 14:15:57 -0800

    Added owner for combat weapon.

--- util/BaseCombatWeapon.h
+++ util/BaseCombatWeapon.h
@@ -19,6 +19,10 @@ public:
 		return getPtr<int>(CLIP_TYPES[i]);
 	}
 
+	edict_t *getOwner() {
+		return getEntity("m_hOwner");
+	}
+
 private:
 	static const char* CLIP_TYPES[2];
 	static const char* AMMO_TYPES[2];
